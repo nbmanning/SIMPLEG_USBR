444b31e7da1f1638597d96dd64275c06aa881228
Nick Manning <nmannin4@kent.edu>
2023-08-23 16:08:55 -0400

    add us data prep script for changing usda data to prep the shock

---
+++ dataprep_US.R
@@ -0,0 +1,24 @@
+
+### To-Do: ###
+# Start Script by changing title, author, etc. 
+
+# title: dataprep_US.R
+# author: Nick Manning
+# purpose: Import and clean muncipality level geospatial and tabular land cover
+# data for BR and the Cerrado
+
+# creation date: 7/27/23
+# last updated: August 2023
+
+# links: 
+## BR GeoTiff Download: https://brasil.mapbiomas.org/en/colecoes-mapbiomas-1?cama_set_language=en
+## Collection 7.1 Download: https://brasil.mapbiomas.org/en/statistics
+
+
+# Requires: 
+## Crosswalk CSV for MapBiomas --> SIMPLE-G
+## GeoTiff downloaded from MapBiomas (link provided)
+## (in-progress) Land Transition Excel sheet downloaded from MapBiomas (link provided)
+
+# Steps (check meeting notes): 
+#
