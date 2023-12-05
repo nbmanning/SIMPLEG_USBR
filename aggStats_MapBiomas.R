241a5b961a10be760b0bcfbc84a84b52e2a69d57
Nick Manning <nmannin4@kent.edu>
2023-12-05 13:39:44 -0500

    add quick script to get BR and Cerrado Agg Stats

---
+++ aggStats_MapBiomas.R
@@ -0,0 +1,28 @@
+# Title: aggStats_MapBiomas.R
+# Purpose: Calculate aggregated transition stats from MapBiomas to compare to our cropland area changes
+
+# Creation Date: 12/4/23
+# Last edited: Dec 2023
+
+# NOTE: uses a bunch of code from ""
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
+
+# 0: Load Libraries & Set Constants -----
+
+## Libraries 
+
+## Constants 
+
+## Shapefiles 
+
+# 1: Load in MapBiomas Transition ------
+
+## 1.1: Tidy
+
+# 2: Brazil ------
+
+# 3: Cerrado --------
+
+
+
