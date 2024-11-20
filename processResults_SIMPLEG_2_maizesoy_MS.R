0325a8407903c1c1e64b3d85811c3b37d9691e05
Nick Manning <nmannin4@kent.edu>
2024-11-20 08:22:18 -0500

    clean table function successfully integrated with MS code :)

--- processResults_SIMPLEG_2_maizesoy_MS.R
+++ processResults_SIMPLEG_2_maizesoy_MS.R
@@ -373,6 +373,62 @@ F_calc_pct_change <- function(final, raw_ch){
   print(paste0("% Change is: ", pct_change, "%"))
 }
 
+# create function 
+F_clean_summary_tables <- function(area_name, pct){
+  
+  csv2 <- read.csv(file = paste0(folder_results, "summary_tables/table_", area_name, pct, "_", date_string_nodash, ".csv"))
+  
+  df2 <- csv2  %>% 
+    select(-X) %>% 
+    # remove all whitespace
+    mutate(across(where(is.character), ~ str_replace_all(., " ", ""))) %>% 
+    # rename by removing the "X.." or "X." 
+    rename_with(~ str_replace_all(., "X\\.\\.?", "")) %>% 
+    # split the first column to get the stats
+    separate_wider_delim(new_QLAND, delim = ":", names = c("stat", "new_QLAND"), too_few = "align_start") %>%
+    # remove everything before the colon (including the colon)
+    mutate(across(where(is.character), ~ str_replace(., "^.*?:", ""))) %>% 
+    # convert to numeric
+    mutate(across(!stat, as.numeric)) %>% 
+    # get rid of the total new land columns - we're only interested in change
+    select(-c(new_QLAND, new_QCROP, new_LND_MAZ, new_LND_SOY)) %>%
+    # add region column 
+    mutate(reg = area_name) %>% 
+    # rename columns for easy export
+    rename(
+      #"new_QLAND" new_QCROP new_LND_MAZ new_LND_SOY
+      "Percent Change in Total Land Area" = "pct_QLAND",
+      "Raw Change in Total Land Area" = "rawch_QLAND", 
+      "Percent Change in Total Crop Production" = "pct_QCROP",
+      "Raw Change in Total Crop Production" = "rawch_QCROP",
+      
+      "Percent Change in Maize Area" = "pct_LND_MAZ",
+      "Raw Change in Maize Area" = "rawch_MAZ", 
+      "Percent Change in Soy Area" = "pct_LND_SOY",
+      "Raw Change in Soy Area" = "rawch_SOY",
+      "_____" = "stat",
+      "region" = "reg")
+  
+  rio::export(df2, file = paste0(folder_results, "summary_tables/cleantable_", area_name, pct, "_", date_string_nodash, ".xlsx"))
+  
+  ## Part 2: AutoGenerate COlumn Widths ##
+  # load xlsx file 
+  wb <- loadWorkbook(paste0(folder_results, "summary_tables/cleantable_", area_name, pct, "_", date_string_nodash, ".xlsx"))
+  # get sheet names 
+  wb_sheet <- names(wb)
+  # Adjust column widths based on column name lengths
+  for (sheet in wb_sheet) {
+    # Get column names
+    col_names <- colnames(read.xlsx(wb, sheet))
+    
+    # Set column widths to match the length of each column name
+    widths <- nchar(col_names) + 2 # Adding some extra space for readability
+    setColWidths(wb, sheet, cols = 1:length(col_names), widths = widths)
+  }
+  
+  # Save the updated workbook
+  saveWorkbook(wb, paste0(folder_results, "summary_tables/cleantable_", area_name, pct, "_", date_string_nodash, ".xlsx"), overwrite = TRUE)
+}
 
 # Fxn to get summary of data, call the violin fxn, and plot a basic map
 F_EDA <- function(r_aoi, area_name){  
@@ -402,6 +458,11 @@ F_EDA <- function(r_aoi, area_name){
   write.csv(table_area_10e6, file = paste0(folder_results, "summary_tables/", 
                                       "table_", area_name, pct, "_10e6_",  date_string_nodash, ".csv"))
   
+  
+  # clean tables 
+  F_clean_summary_tables(area_name, pct = pct)
+  
+  ## Change Section ##
   print("Totals for Casc. Effect Graph and for Total Change")
   
   # Print total change values then calculate % Change
