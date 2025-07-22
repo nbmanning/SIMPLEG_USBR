d7b0bcfea6faf3c1611e1cfa9ff0c31d10fc5a3e
Nick Manning <nmannin4@kent.edu>
2025-07-22 09:10:10 -0400

    START OF FINAL CLEANING: small edits to v4 from v2 and readme

--- US_Drought_shock_v4.R
+++ US_Drought_shock_v4.R
@@ -1,21 +1,44 @@
+# Title: US_Drought_shock_v4.R
 
-# US_Drought_shock_v4
-# This script calculates the US 2012 crop shock for use in SIMPLE-G-Global
-# v4:
-# -- calculate the insurance indemnity payments from only drought and heat per insured acre (A = Loss/Acre)
-## -- note: the difference here is that we filter to drought+heat, corn+soybeans, and months of drought (may-october)
-# -- calculates the expected 2012 crop sale per acre (B = Sale/Acre)
-# -- calculates the loss rate (C = A/B)
-# -- For each county, we assume the non-insured acres have the same loss rate
-# -- construct the shock HAR file for use in SIMPLE-G
+# Purpose: This script calculates the US 2012 crop shock for use in SIMPLE-G-Global
 
-# rm(list = ls())
+# Initial script by: Iman Haqiqi
+# Initial date: ~October 2024
+
+# Edited by: Nick Manning 
+# Last edited: July 2025
+
+# REQUIRES:
+## US_Drought_Shock folder with
+### Coords.csv
+### colsom12.txt
+### AGLAND_CROPLAND_ACRES.csv
+### AGLAND_CROP_INSURANCE_ACRES_FIPS.csv
+### CROP_TOTALS_SALES_USD_FIPS.csv
+### cb_2018_us_state_500k.shp
+
+# OUTPUTS:
+## 
+
+# NOTES:
+## 
+
+# Next:
+## Re-do colsom12.txt path
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# (0) Load libraries --------
+rm(list = ls())
+getwd()
+
 # load required libraries for raster analysis
-library(terra)
+library(terra) # load Coords.csv
+library(dplyr) # df loading and editing
 
-# (1) create a data frame with FIPS and XY coordinates and SIMPLE-G grid IDs ----
+# (1) Create a data frame with FIPS and XY coordinates and SIMPLE-G grid IDs ----
 # US coordinates and FIPS
-us.xy.file = "../US_Drought_Shock_v2/Coords.csv" #NOTE: re-name folder to "../US_Drought_Shock_v2" after moving this code to the "Code" folder 
+us.xy.file = "../US_Drought_Shock_v4/Coords.csv" #NOTE: re-name folder to "../US_Drought_Shock_v4" after moving this code to the "Code" folder 
 tmp = read.csv(us.xy.file, header=T, sep=",")
 
 # re-generate coordinates to ensure decimal accuracy
@@ -28,13 +51,14 @@ plot(tmp.ras)
 
 us.xy = tmp[c("x","y","FIPS", "VCRP_2010", "QLND_2010")]
 
-# (2) create a data frame of loss by FIPS for 2012 ----
+# (2) Create a data frame of loss by FIPS for 2012 ----
+
 # read the USDA RMA Causes of Loss
-loss.file = "../US_Drought_Shock_v2/colsom_2012/colsom12.txt" #NOTE: needed to re-name folder here
+loss.file = "../US_Drought_Shock_v4/colsom_2012/colsom12.txt" #NOTE: needed to re-name folder here
 loss.dt = read.csv(loss.file, header =F, sep="|")
 
 #read the column labels
-loss.id = "../US_Drought_Shock_v2/Codes.csv"
+loss.id = "../US_Drought_Shock_v4/Codes.csv"
 id = read.csv(loss.id, header =F, sep=",")
 
 names(loss.dt) = id$V1
@@ -46,19 +70,28 @@ loss.dt$FIPS = loss.dt$State_Code * 1000 + loss.dt$County_Code
 unique(loss.dt$Cause_Description)
 
 # calculate heat separately to compare
-loss.dt <- loss.dt[loss.dt$Cause_Description == "Drought" |
-                     loss.dt$Cause_Description == "Heat"   ,]
 
+# first run this to get rid of empty spaces in comm. ID
+# regex means all spaces (\\s) after the last letter (+$)
+loss.dt <- loss.dt %>%
+  mutate(Commodity_ID = gsub("\\s+$", "", Commodity_ID))
+
+# filter to calculate losses only from drought+heat, corn/soy, and months of the drought (may-oct)
+loss.dt <- loss.dt %>% 
+  filter(Cause_Description %in% c("Drought", "Heat")) %>%
+  filter(Commodity_ID %in% c("CORN", "SOYBEANS")) %>% 
+  filter(Month_Name %in% c("MAY", "JUN", "JUL", "AUG", "SEP", "OCT"))
+
+# select only indemnity and FIPS amounts
 loss.df = loss.dt[c("FIPS", "Indemnity_Amount")]
 loss.aggr = aggregate( . ~ FIPS, data =loss.df, FUN= sum, na.rm=T ) ## Q: Doesn't this technically include ALL forms of indemnity loss?
 summary(loss.aggr)
 
 
-# (3) create a data frame of insured acres ----
-library(dplyr)
+# (3) Create a data frame of insured acres ----
 
 ## read the insured acres from USDA-NASS Census of AG ----
-my.file = "../US_Drought_Shock_v2/AGLAND_CROP_INSURANCE_ACRES_FIPS.csv"
+my.file = "../US_Drought_Shock_v4/AGLAND_CROP_INSURANCE_ACRES_FIPS.csv"
 my.dt = read.csv(my.file, header =T, sep=",")
 my.df <- my.dt %>% mutate_all(as.numeric)
 summary(my.df)
@@ -69,7 +102,7 @@ head(insured.acres)
 
 
 ## read the cropland acres from USDA-NASS Census of AG ----
-my.file = "../US_Drought_Shock_v2/AGLAND_CROPLAND_ACRES.csv"
+my.file = "../US_Drought_Shock_v4/AGLAND_CROPLAND_ACRES.csv"
 my.dt = read.csv(my.file, header =T, sep=",")
 my.df <- my.dt %>% mutate_all(as.numeric)
 summary(my.df)
@@ -79,7 +112,7 @@ names(crop.acres) = c("FIPS", "Crop.Acres")
 head(crop.acres)
 
 ## read the crop sales from USDA-NASS Census of AG ----
-my.file = "../US_Drought_Shock_v2/CROP_TOTALS_ SALES_USD_FIPS.csv" #NOTE: Space in filename here - in the source so it works, but might want to change that
+my.file = "../US_Drought_Shock_v4/CROP_TOTALS_SALES_USD_FIPS.csv"
 my.dt = read.csv(my.file, header =T, sep=",")
 my.df <- my.dt %>% mutate_all(as.numeric)
 summary(my.df)
@@ -87,7 +120,7 @@ summary(my.df)
 sales.df = my.df
 head(sales.df)
 
-##  how much the 2012 sale would be without drought? ----
+## how much the 2012 sale would be without drought? ----
 ## average of 2007-2017 or observed sales in 2012, which one is bigger
 sales.df$Expected_Sale = pmax(sales.df$X2012, 
                               (sales.df$X2007 + sales.df$X2017)/2,
@@ -97,7 +130,7 @@ sales.df$Expected_Sale = pmax(sales.df$X2012,
 crop.sales = sales.df[c("FIPS", "Expected_Sale")]
 
 
-# (4) merge data sets and calculate the rates (averages) ---- 
+# (4) Merge data sets and calculate the rates (averages) ---- 
 fips.df = merge(loss.aggr, insured.acres, by = "FIPS", all.x=T) 
 fips.df = merge(fips.df,      crop.acres, by = "FIPS", all.x=T)
 fips.df = merge(fips.df,      crop.sales, by = "FIPS", all.x=T)
@@ -109,6 +142,9 @@ fips.df$AvgSalesAcre = fips.df$Expected_Sale   / fips.df$Crop.Acres
 fips.df$LossRate = fips.df$AvgIndemAcre   / fips.df$AvgSalesAcre
 
 summary(fips.df)
+
+# Avg. Loss Rate before cutting off at 0.99 
+# print(paste("Average Loss Rate (c+s):", mean(fips.df$LossRate, na.rm = T)))
 
 ## create a data-frame for all XY assuming uniform rates in each county FIPS ----
 df.xyz = merge(us.xy, fips.df[c("FIPS", "LossRate", "AvgIndemAcre", "AvgSalesAcre")], by = "FIPS", all.x=T)
@@ -127,6 +163,16 @@ plot(s)
 r = s$LossRate
 r[r > 0.99] = 0.99
 
+# Avg. Loss Rate after cutting off at 0.99 
+print(paste("Average Loss Rate (c+s, post-truncate):", global(r$LossRate, fun = "mean", na.rm = T)))
+
+# 
+fips2 <- fips.df$LossRate
+fips2[fips2 > 0.99] = 0.99
+fips2[is.na(fips2)] = 0
+mean(fips2, na.rm = T) # 0.1703 before setting NA to 0; 0.1692 after setting all NA to 0
+
+
 # save df's for: 
 ## df.xyz = the XY loss rates that get converted to raster; don't need fips.df bc fips.df feeds into this
 ## loss.aggr = the variable we're going to change by filtering only to drought and heat   
@@ -138,12 +184,12 @@ save(df.xyz, loss.aggr,
 save(r, file = "../Data_Derived/shock_v4_r99.RData")
 
 
-# (5) plot the shock ----
+# (5) Plot the shock ----
 plot(r)
 
 ## change the projection
 u = terra::project(r, "+init=epsg:2163")
-v = vect("../US_Drought_Shock_v2/cb_2018_us_state_500k/cb_2018_us_state_500k.shp") #NOTE: Need to copy/paste the US shp folder to v2
+v = vect("../US_Drought_Shock_v4/cb_2018_us_state_500k/cb_2018_us_state_500k.shp") #NOTE: Need to copy/paste the US shp folder to v2
 v = crop(v,s)
 w = terra::project(v, "+init=epsg:2163")
 
@@ -162,7 +208,11 @@ text(w, "STUSPS", cex=0.7)
 
 # Plot using Tidyterra
 library(tidyterra)
+
 # Define breaks and colors using RColorBrewer
+library(RColorBrewer)
+library(ggplot2)
+
 breaks <- c(0, 5, 10, 50, 99)
 #colors <- c("#fecc5c", "#fd8d3c", "#f03b20", "#bd0026")
 colors <- brewer.pal(n = length(breaks) - 1, name = "YlOrRd")
@@ -192,7 +242,7 @@ ggplot()+
 ggsave("../Figures/SIMPLEG_Shock_v4.png",
        height = 10, width = 15, dpi = 300)
 
-# (6) shock for SIMPLE-G global  ----
+# (6) Shock for SIMPLE-G global  ----
 xygID  <- rast("../US_Drought_Shock_v4/grid_id_xyg.tif")
 plot(xygID)
 
@@ -221,7 +271,7 @@ txt <- paste0(nGrid, ' real row_order header "',v,'" longname "US 2012 Drought S
 
 txt.file = paste0(v,".txt")
 
-write(txt, txt.file) # NOTE: Need to manually move y12b from the working directory (for me, "Code") to US_Drought_Shock_v2
+write(txt, txt.file) # NOTE: Need to manually move y12b from the working directory (for me, "Code") to US_Drought_Shock_v4
 
 my.var = "LossRate"
 
